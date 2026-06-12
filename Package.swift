7416215fa96c63f46c5684c0dca8306da4b75a5a
iflamed <iflamed@gmail.com>
2026-06-12 17:23:06 +0800

    release: HuanGeSdk 1.0.0

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "HuanGeSdk",
-            url: "https://github.com/caitunai/HuanGe-iOS-sdk/releases/download/0.4.0/HuanGeSdk-0.4.0.xcframework.zip",
-            checksum: "53ee7145f6293a11daf475ae1c372a621662e36e5225603d8833a34b5aa51615"
+            url: "https://github.com/caitunai/HuanGe-iOS-sdk/releases/download/1.0.0/HuanGeSdk-1.0.0.xcframework.zip",
+            checksum: "62b94f746f0353f0b97f358d764a573907ff4d17c162595afef005a5688a075f"
         )
     ]
 )
